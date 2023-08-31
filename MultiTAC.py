be4473a2af4a4a8e4291388b6e0cc25521eefd0a
Edgardo Silva <e.silva.plaza@gmail.com>
2023-08-31 10:34:59 -0400

    before Threading

--- MultiTAC.py
+++ MultiTAC.py
@@ -13,23 +13,24 @@ import screeninfo
 from BrushGui.axidraw import brush
 from pyqtconsole.console import PythonConsole
 import pandas as pd
+import numpy as np
 
 from PyQt6 import QtWidgets, QtCore
 from PyQt6.uic import loadUi
-from PyQt6.QtCore import QAbstractTableModel
-from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
 from PyQt6.QtWidgets import QMainWindow, QApplication, QInputDialog, QFileDialog
 
 
 class AxiDraw(threading.Thread):
-    def __init__(self):
+    def __init__(self, q_in, q_out):
         threading.Thread.__init__(self)
-        self.serial_port = 'COM3'
+        self.serial_port = None
         self.running = True
         self.serial_status = False
         self.running = True
-        # self.q_in = q_in
-        # self.q_out = q_out
+        self.q_in = q_in
+        self.q_out = q_out
 
     def stop(self):
         self.running = False
@@ -38,69 +39,85 @@ class AxiDraw(threading.Thread):
 
     def run(self):
         while self.running:
-            continue
-
-    def connectDevice(self):
-        port = brush.findPort()  # Success!! Axidraw founded in ComPort
-        print(port)
-        if port:
-            self.serial_port, self.serial_status = brush.openPort(port)
-            if self.serial_port:
-                if self.serial_status:
-                    return self.serial_port.name
-                    # self.q_out.put(self.serial_port.name, self.serial_status)
-                    # print(self.serial_port.name)
+            msg = self.q_in.get()
+            if msg == 'open_port':
+                port = brush.findPort()  # Success!! Axidraw founded in COMPort
+                if port:
+                    self.serial_port, self.serial_status = brush.openPort(port)
+                    if self.serial_port:
+                        if self.serial_status:
+                            self.q_out.put(self.serial_port.name, self.serial_status)
+                            # return self.serial_port.name
+                        else:
+                            self.q_out.put(self.serial_port.name)
+                            # return self.serial_port.name
+                    else:
+                        print('Could not find a port with an AxiDraw connected')
+                        self.q_out.put('OK')
                 else:
-                    return self.serial_port.name
-                    # self.q_out.put(self.serial_port.name)
-            else:
-                print('Could not find a port with an AxiDraw connected')
-                # self.q_out.put('error_1')
-        else:
-            print('Could not find a port with an AxiDraw connected')
-            # self.q_out.put('error_1')
+                    print('Could not find a port with an AxiDraw connected')
+                    self.q_out.put('OK')
 
-    def startDevice(self, data):
-        print(self)
-        print(self.serial_port)
-        print(data)
-        # brush.sendEnableMotors(self.serial_port, 2)
-        # time.sleep(0.1)
-        # print(data)
-        """
-        trial = eval(message.split('_')[1])
-        length, speed, direction = trial
-        print(f'Brushing trial {trial}')
+            if msg == 'close_port':
+                port = brush.findPort()
+                if port is None:
+                    print('Could not find a port with an AxiDraw connected')
+                else:
+                    print('Closing port', port)
+                    self.serial_port = brush.closePort(port)
 
-        # linear movement
-        if direction == 'R':
-            brush.pen_down(self.serial_port)
-            brush.move_x(self.serial_port, length, speed)
-            brush.pen_up(self.serial_port)
-            brush.move_x(self.serial_port, -length, 3)
-        elif direction == 'L':
-            brush.pen_down(self.serial_port)
-            brush.move_x(self.serial_port, -length, 3)
-            brush.pen_up(self.serial_port)
-            brush.move_x(self.serial_port, length, speed)
+            if msg[0] == 'execute order 66':
+                brush.sendEnableMotors(self.serial_port, 2)
+                speed = msg[1]
+                direction = msg[2]
+                distance = msg[3]
+                # print('Brushing at ' + str(speed) + ' cm/s')
+                if direction == 'Left to Right':
+                    brush.pen_down(self.serial_port)
+                    brush.move_x(self.serial_port, distance, speed)
+                    brush.pen_up(self.serial_port)
+                    brush.move_x(self.serial_port, -distance, 3)
+                elif direction == 'Right to Left':
+                    brush.pen_down(self.serial_port)
+                    brush.move_x(self.serial_port, -distance, 3)
+                    brush.pen_up(self.serial_port)
+                    brush.move_x(self.serial_port, distance, speed)
 
-        # circular movement
-        elif direction == 'CW':
-            brush.pen_down(self.serial_port)
-            brush.move_circular(self.serial_port, speed, 0.67, 1, direction)
-            brush.pen_up(self.serial_port)
-        elif direction == 'ACW':
-            brush.pen_down(self.serial_port)
-            brush.move_circular(self.serial_port, speed, 0.67, 1, direction)
-            brush.pen_up(self.serial_port)
-        brush.sendDisableMotors(self.serial_port)
-        """
+                    # circular movement
+                elif direction == 'CW':
+                    brush.pen_down(self.serial_port)
+                    brush.move_circular(self.serial_port, speed, 0.67, 1, direction)
+                    brush.pen_up(self.serial_port)
+                elif direction == 'ACW':
+                    brush.pen_down(self.serial_port)
+                    brush.move_circular(self.serial_port, speed, 0.67, 1, direction)
+                    brush.pen_up(self.serial_port)
+
+                brush.sendDisableMotors(self.serial_port)
+                self.q_out.put('OK')
 
 
-axidraw = AxiDraw()
+q_to_ad = queue.Queue()
+q_from_ad = queue.Queue()
+axidraw = AxiDraw(q_to_ad, q_from_ad)
 
 axidraw.daemon = True
 axidraw.start()
+
+
+class vasPage(QMainWindow):
+    def __init__(self):
+        super(vasPage, self).__init__()
+        loadUi("gui/vas.ui", self)
+        self.vasSlider.valueChanged.connect(self.updateDisplay)
+        self.vasSubmit.clicked.connect(self.getValues)
+
+    def getValues(self):
+        print(self.vasSlider.value())
+        self.close()
+
+    def updateDisplay(self):
+        self.vasCurrent.display(self.vasSlider.value())
 
 
 class MainUI(QMainWindow):
@@ -135,18 +152,20 @@ class MainUI(QMainWindow):
         self.updateExperiment(data)
 
     def updateExperiment(self, data):
-        self.MovementDirection.setCurrentText(data[1][0])
-        self.BodySite.setCurrentText(data[1][1])
-        self.velocitiesInput.setText(data[1][2])
-        self.distanceInput.setText(data[1][3])
-        self.repetitionsInput.setText(data[1][4])
-        self.intertrialIntput.setText(data[1][5])
-        self.vastimeInput.setText(data[1][6])
+        self.movementPath.setCurrentText(data[1][0])
+        self.bodySite.setCurrentText(data[1][1])
+        self.movementDirection.setCurrentText(data[1][2])
+        self.velocitiesInput.setText(data[1][3])
+        self.distanceInput.setText(data[1][4])
+        self.repetitionsInput.setText(data[1][5])
+        self.intertrialIntput.setText(data[1][6])
+        self.vastimeInput.setText(data[1][7])
 
     def saveExperimentAction(self):
         # save a experiment file
-        data = [self.MovementDirection.currentText(),
-                self.BodySite.currentText(),
+        data = [self.movementPath.currentText(),
+                self.bodySite.currentText(),
+                self.movementDirection.currentText(),
                 self.velocitiesInput.displayText(),
                 self.distanceInput.displayText(),
                 self.repetitionsInput.displayText(),
@@ -163,10 +182,10 @@ class MainUI(QMainWindow):
         if filename[0] == "":
             return
 
-        indexRow = ["Movement", "Site",
-                    "Velocity", "Distance",
-                    "Repetitions", "VasTime",
-                    "InterTime"]
+        indexRow = ["Path", "Site",
+                    "Movement", "Velocity",
+                    "Distance", "Repetitions",
+                    "VasTime", "InterTime"]
         dataToCsv = pd.DataFrame(data, index=indexRow)
         dataToCsv.to_csv(str(filename[0]), header=False)
 
@@ -218,18 +237,41 @@ class MainUI(QMainWindow):
         self.handInput.setCurrentIndex(0)
 
     def connectDevice(self):
-        self.printToConsole('Attempting to connect...')
-        AxiDraw.connectDevice(axidraw)
+        q_to_ad.put('open_port')
+        answer = q_from_ad.get()
+        print(answer)
 
     def startExperiment(self):
-        self.printToConsole('Starting Experiment')
+        trialsRnd = []
         data = self.getExperimentData()
-        AxiDraw.startDevice(axidraw, data)
 
+        direction = data[2]
+        trials = data[3].split(";")
+        distance = int(data[4])
+        reps = int(data[5])
+        interTime = int(data[6])
+        VasTime = int(data[7])
+
+        trials = [eval(i) for i in trials]
+        for i in range(0, reps):
+            trialsRnd.append(random.sample(trials, len(trials)))
+        """
+        trialsRnd
+        timeCount = len(trials) * reps
+        count = 0
+
+        for cycle in trialsRnd:
+            for trial in cycle:
+                self.printToConsole('Brushed at ' + str(trial) + ' cm/s')
+                q_to_ad.put(['execute order 66', trial, direction, distance])
+                answer = q_from_ad.get()
+                print(answer)
+        """
     def getExperimentData(self):
-        self.printToConsole('Starting Experiment')
-        data = [self.MovementDirection.currentText(),
-                self.BodySite.currentText(),
+        # self.printToConsole('Starting Experiment')
+        data = [self.movementPath.currentText(),
+                self.bodySite.currentText(),
+                self.movementDirection.currentText(),
                 self.velocitiesInput.displayText(),
                 self.distanceInput.displayText(),
                 self.repetitionsInput.displayText(),
@@ -237,25 +279,16 @@ class MainUI(QMainWindow):
                 self.vastimeInput.displayText()]
         return data
 
-        """
-        q_to_ad.put('open_port')
-        answer = q_from_ad.get()
-        if answer not in 'error_3':
-            window['status2'].Update(f'MultiTAC Power ON')
-            Powered = True
-        else:
-            print('Failed to connect: MultiTAC power OFF')
-            window['status2'].Update(f'MultiTAC Power OFF', text_color='DarkRed')
-        if answer not in ('error_1', 'error_2', 'error_3'):
-            window['status'].Update(f'Connected to {answer}', text_color='Blue')
-            window['status2'].Update(f'MultiTAC Power ON', text_color='Blue')
-            # q_to_ad.put('query_power')
-            Connected = True
-        """
+    def getVas(self):
+        self.main = vasPage()
+        self.main.show()
 
 
 if __name__:
-    app = QApplication(sys.argv)
-    qt_app = MainUI()
-    qt_app.show()
-    app.exec()
+    debugger = True
+
+    if debugger:
+        app = QApplication(sys.argv)
+        qt_app = MainUI()
+        qt_app.show()
+        app.exec()
